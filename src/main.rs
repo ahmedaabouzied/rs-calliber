e7e8563cd3b729194b3752e7e7172807ed4e90c3
Ahmed Abouzied <email@aabouzied.com>
2024-09-10 19:02:57 +0200

    Play the sine wave sound in sync with the graph
    Signed-off-by: Ahmed Abouzied <email@aabouzied.com>

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,47 @@
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{Data, FromSample, Sample, SampleFormat};
+// GUI
 use eframe::egui;
 use egui_plot::{Line, Plot, PlotPoints};
-use std::f32::consts::PI;
-use std::sync::{Arc, Mutex};
+
+// Audio
+use rodio::{source::SineWave, source::Source, OutputStream, Sink};
+use std::thread;
 use std::time::Instant;
 
 // Constants
 const A4_FREQ: f32 = 440.0;
 const SAMPLE_RATE: f32 = 44100.0; // Standard audio sample rate
-const DURATION: f32 = 15.0; // 15 seconds for the A4 note
+const DURATION: f32 = 5.0; // 15 seconds for the A4 note
 
-fn generate_sine_wave(frequency: f32, duration: f32, sample_rate: f32) -> Vec<f32> {
-    let total_samples = (duration * sample_rate) as usize;
-    (0..total_samples)
-        .map(|x| {
-            let t = x as f32 / sample_rate;
-            (t * frequency * 2.0 * PI).sin()
-        })
-        .collect()
+fn generate_sine_wave(frequency: f32) -> SineWave {
+    return SineWave::new(frequency);
 }
 
 struct MyEguiApp {
-    sine_wave: Vec<f32>,
+    sine_wave: SineWave,
+    duration: f32,
     is_playing: bool,
     started_sound: bool,
     start_time: Instant,
-    last_time: f32,
     zoom_factor: f32,
     x_offset: f32,
     points_vector: Vec<[f64; 2]>,
 }
 
 impl MyEguiApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let sine_wave = generate_sine_wave(A4_FREQ, DURATION, SAMPLE_RATE);
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        let sine_wave = generate_sine_wave(A4_FREQ);
         let start_time = Instant::now();
         let is_playing = false;
         let started_sound = false;
-        let last_time = 0.0;
         let zoom_factor = 1.0;
         let x_offset = 0.0;
         let points_vector = vec![];
         Self {
             sine_wave,
+            duration: DURATION,
             is_playing,
             started_sound,
             start_time,
-            last_time,
             zoom_factor,
             x_offset,
             points_vector,
@@ -64,44 +58,22 @@ impl MyEguiApp {
     }
 
     fn start_sound(&mut self) {
-        if !self.started_sound {
+        if self.started_sound {
             return;
         }
         self.started_sound = true;
-        let host = cpal::default_host();
-        let device = host
-            .default_output_device()
-            .expect("default output device not found");
-        let mut suported_configs_range = device
-            .supported_output_configs()
-            .expect("supported configs range not found");
-        let supported_config = suported_configs_range
-            .next()
-            .expect("no supporte configs")
-            .with_max_sample_rate();
-        let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
-        let sample_format = supported_config.sample_format();
-        let config = supported_config.into();
-        let stream = match sample_format {
-            SampleFormat::F32 => {
-                device.build_output_stream(&config, write_silence::<f32>, err_fn, None)
-            }
-            SampleFormat::I16 => {
-                device.build_output_stream(&config, write_silence::<i16>, err_fn, None)
-            }
-            SampleFormat::U16 => {
-                device.build_output_stream(&config, write_silence::<u16>, err_fn, None)
-            }
-            sample_format => panic!("Unsupported sample format '{sample_format}'"),
-        }
-        .unwrap();
-        stream.play().unwrap();
-    }
-}
-
-fn write_silence<T: Sample>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
-    for sample in data.iter_mut() {
-        *sample = Sample::EQUILIBRIUM;
+        // Play the sound in a separate thread
+        let duration = self.duration;
+        thread::spawn(move || {
+            let sine_wave = SineWave::new(A4_FREQ);
+            // Play the sound for 2 seconds through the speakers
+            // Get an output stream handle to the default physical sound device
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            let sink = Sink::try_new(&stream_handle).unwrap();
+            // Play the sound directly on the device
+            sink.append(sine_wave.take_duration(std::time::Duration::from_secs(duration as u64)));
+            sink.sleep_until_end();
+        });
     }
 }
 impl eframe::App for MyEguiApp {
@@ -155,7 +127,11 @@ impl eframe::App for MyEguiApp {
 
                 // Plot the sine wave over time
                 let samples_to_show = (max_time * SAMPLE_RATE) as usize;
-                let sine_wave_segment = &self.sine_wave[..samples_to_show];
+                let sine_wave_segment = self
+                    .sine_wave
+                    .clone()
+                    .take(samples_to_show)
+                    .collect::<Vec<f32>>();
 
                 let points: Vec<[f64; 2]> = sine_wave_segment
                     .iter()
@@ -170,6 +146,7 @@ impl eframe::App for MyEguiApp {
                 // Stop playing after 15 seconds
                 if elapsed >= DURATION {
                     self.is_playing = false;
+                    self.started_sound = false;
                 }
             }
 
@@ -181,11 +158,47 @@ impl eframe::App for MyEguiApp {
     }
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
+    let _ = eframe::run_native(
         "Caliber",
         native_options,
         Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))),
     );
+}
+
+// WebAssembly entry point
+#[cfg(target_arch = "wasm32")]
+fn wasm_main() {
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen(start)]
+    pub fn start() -> Result<(), JsValue> {
+        let mut app = MyEguiApp::new(&eframe::CreationContext {
+            web_info: eframe::WebInfo {
+                canvas_id: "the_canvas_id".to_owned(),
+            },
+        });
+        egui_web::start("the_canvas_id", Box::new(app))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rodio::{source::SineWave, source::Source, OutputStream, Sink};
+
+    #[test]
+    fn test_make_a4_sound() {
+        let sine_wave = SineWave::new(A4_FREQ).take_duration(std::time::Duration::from_secs(2));
+        // Play the sound for 2 seconds through the speakers
+        // // Get an output stream handle to the default physical sound device
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+        // Load a sound from a file, using a path relative to Cargo.toml
+        // Play the sound directly on the device
+        sink.append(sine_wave);
+        sink.sleep_until_end();
+    }
 }
