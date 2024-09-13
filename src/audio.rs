58f400f8273e87255ee037519d0e261df053fd27
Ahmed Abouzied <email@aabouzied.com>
2024-09-13 03:02:28 +0200

    Handle the default input and output devices
    Signed-off-by: Ahmed Abouzied <email@aabouzied.com>

--- src/audio.rs
+++ src/audio.rs
@@ -19,15 +19,38 @@ pub fn get_output_devices() -> Result<cpal::OutputDevices<cpal::Devices>, cpal::
     host.output_devices()
 }
 
-pub fn capture_input(input_device_name: String, sample_rate: f32, duration: f32, tx: Sender<f32>) {
-    let input_device = self::get_input_devices()
-        .unwrap()
-        .find(|d| d.name().unwrap() == input_device_name)
-        .unwrap();
-    let config_range = input_device.default_input_config().unwrap();
-    let data = Arc::new(Mutex::new(Vec::new()));
+fn select_input_device(device_name: String) -> cpal::Device {
+    match device_name.as_str() {
+        "Default" => {
+            let host = cpal::default_host();
+            host.default_input_device().unwrap()
+        }
+        _ => self::get_input_devices()
+            .unwrap()
+            .find(|d| d.name().unwrap() == device_name)
+            .unwrap(),
+    }
+}
 
-    let data_clone = Arc::clone(&data);
+fn select_output_device(device_name: String) -> cpal::Device {
+    match device_name.as_str() {
+        "Default" => {
+            let host = cpal::default_host();
+            host.default_output_device().unwrap()
+        }
+        _ => self::get_output_devices()
+            .unwrap()
+            .find(|d| d.name().unwrap() == device_name)
+            .unwrap(),
+    }
+}
+
+pub fn capture_input(input_device_name: String, sample_rate: f32, duration: f32, tx: Sender<f32>) {
+    let input_device = select_input_device(input_device_name);
+    let config_range = input_device.default_input_config().unwrap();
+
+    let data = Arc::new(Mutex::new(Vec::new())); // Will live along this function.
+    let data_clone = Arc::clone(&data); // To be moved to a lambda.
 
     let input_stream = input_device
         .build_input_stream(
@@ -35,7 +58,7 @@ pub fn capture_input(input_device_name: String, sample_rate: f32, duration: f32,
             move |data: &[f32], _: &cpal::InputCallbackInfo| {
                 let mut locked_data = data_clone.lock().unwrap();
                 locked_data.extend_from_slice(data);
-            },
+            }, // data_clone is dropped here.
             move |err| {
                 eprintln!("An error occurred on the input stream: {}", err);
             },
@@ -55,15 +78,9 @@ where
     f32: FromSample<S::Item>,
     S::Item: Sample + Send,
 {
-    let (_stream, stream_handle) = OutputStream::try_from_device(
-        &self::get_output_devices()
-            .unwrap()
-            .find(|d| d.name().unwrap() == output_device_name)
-            .unwrap(),
-    )
-    .unwrap();
+    let (_stream, stream_handle) =
+        OutputStream::try_from_device(&select_output_device(output_device_name)).unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
-    // Play the sound directly on the device
     sink.append(sound);
     sink.sleep_until_end();
 }
